6a11483b135b893c1c3115ba6722e569c224601b
Frederik Nielsen <fniel22@student.aau.dk>
2023-10-18 09:41:14 +0200

    final version
    Needs a bit of adjusting

--- testcrown.py
+++ testcrown.py
@@ -1,8 +1,12 @@
-import cv2
 import numpy as np
+import cv2
 
 # Load the image
 image = cv2.imread("CroppedDataset/7.jpg")
+
+# Display the original image
+cv2.imshow('Original Image', image)
+cv2.waitKey(0)
 
 # Convert the image to the HSV color space
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -14,61 +18,111 @@ upper_yellow = np.array([35, 210, 200])
 # Create a binary mask that isolates yellow regions
 yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
-# Bitwise-AND the original image with the yellow mask
-result = cv2.bitwise_and(image, image, mask=yellow_mask)
+# Display the yellow mask before closing
+cv2.imshow('Yellow Mask (Before Closing)', yellow_mask)
+cv2.waitKey(0)
 
-# Convert the result to grayscale
-gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+# Apply closing to the mask
+kernel1 = np.ones((5, 5), np.uint8)
+kernel2 = np.ones((3, 3), np.uint8)
 
-# Find contours in the image
-contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+yellow_mask = cv2.morphologyEx(yellow_mask, cv2.MORPH_CLOSE, kernel1, iterations=1)
 
-# Initialize a list to store blobs that match the size criteria
-filtered_blobs = []
+# Display the yellow mask after closing
+cv2.imshow('Yellow Mask (After Closing)', yellow_mask)
+cv2.waitKey(0)
 
-# Define the size range and maximum width and height
-min_area = 16 * 7
-max_area = 20 * 16
-max_width = 23
-max_height = 23
+# Apply erosion to the mask after closing
+yellow_mask = cv2.erode(yellow_mask, kernel2, iterations=1)
 
-# Iterate through the contours
+# Display the yellow mask after erosion
+cv2.imshow('Yellow Mask (After Erosion)', yellow_mask)
+cv2.waitKey(0)
+
+# Find contours in the binary mask (yellow_mask)
+contours, _ = cv2.findContours(yellow_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Define the minimum and maximum area for a blob (in pixels)
+min_blob_area = 30  # Adjust this value
+max_blob_area = 120  # Adjust this value
+
+# Initialize a list to store pruned blobs
+pruned_blobs = []
+
+# Iterate through the contours and prune them based on area
 for contour in contours:
-    # Calculate the bounding rectangle for each contour
+    area = cv2.contourArea(contour)
+
+    if min_blob_area <= area <= max_blob_area:
+        pruned_blobs.append(contour)
+
+# Create a new binary mask with pruned blobs
+pruned_mask = np.zeros_like(yellow_mask)
+cv2.drawContours(pruned_mask, pruned_blobs, -1, 255, thickness=cv2.FILLED)
+
+# Save the pruned mask as an image
+cv2.imshow('PrunedMask.jpg', pruned_mask)
+
+# Create a 5x5 array to store pruned blobs for each block
+block_blobs = [[[] for _ in range(5)] for _ in range(5)]
+
+# Determine the width and height of each block in the image
+block_width = image.shape[1] // 5
+block_height = image.shape[0] // 5
+
+# Define the size of the middle region to exclude (50 pixels from each side)
+middle_size = 50
+
+# Define the size of the border region to exclude (5 pixels from each side)
+border_size = 3
+
+# Iterate through the pruned blobs and add them to the block_blobs array
+for contour in pruned_blobs:
     x, y, w, h = cv2.boundingRect(contour)
-    area = w * h
 
-    # Check if the blob's area is within the specified range and if its width and height are within the maximum limits
-    if min_area <= area <= max_area and w <= max_width and h <= max_height:
-        filtered_blobs.append((x, y, w, h))
+    # Calculate the block position
+    block_x = min(x // block_width, 4)
+    block_y = min(y // block_height, 4)
 
-# Determine the grid size
-grid_size = 5
+    # Check if the blob falls within the middle region of the block
+    if (
+        x + w >= block_x * block_width + middle_size
+        and x <= (block_x + 1) * block_width - middle_size
+        and y + h >= block_y * block_height + middle_size
+        and y <= (block_y + 1) * block_height - middle_size
+    ):
+        continue  # Skip blobs in the middle region
 
-# Initialize a numpy array to store the counts of blobs in each grid cell
-grid_counts = np.zeros((grid_size, grid_size), dtype=np.int32)
+    # Check if the blob falls within the border region of the block
+    if (
+        x <= block_x * block_width + border_size
+        or x + w >= (block_x + 1) * block_width - border_size
+        or y <= block_y * block_height + border_size
+        or y + h >= (block_y + 1) * block_height - border_size
+    ):
+        continue  # Skip blobs in the border region
 
-# Calculate the size of each grid cell
-cell_height = image.shape[0] // grid_size
-cell_width = image.shape[1] // grid_size
+    # Add the pruned blob to the corresponding block
+    block_blobs[block_y][block_x].append(contour)
 
-# Iterate through the filtered blobs and count them in each grid cell
-for x, y, w, h in filtered_blobs:
-    cell_x = x // cell_width
-    cell_y = y // cell_height
-    grid_counts[cell_y, cell_x] += 1
+# Draw red boxes around the pruned blobs on the original image
+for i in range(5):
+    for j in range(5):
+        for contour in block_blobs[i][j]:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-# Display the counts
-print("Grid Counts:")
-print(grid_counts)
+# Display the result (image with red boxes around pruned blobs)
+cv2.imshow('Yellow Objects with Pruned Blobs', image)
 
-result_with_blobs = image.copy()
-for x, y, w, h in filtered_blobs:
-    cv2.rectangle(result_with_blobs, (x, y), (x + w, y + h), (0, 0, 255), 2)  # Red color, thickness = 2
+# Iterate through the 5x5 block_blobs array and count the number of contours in each block
+block_blob_counts = [[len(block_blobs[i][j]) for j in range(5)] for i in range(5)]
 
-# Display the result (image with yellow and blob counts)
-cv2.imshow('Yellow Objects with Blobs', result_with_blobs)
-cv2.imshow('Yellow Objects', result)
+# Print the resulting count array
+print("Block Blob Counts:")
+for row in block_blob_counts:
+    print(row)
 
+# Release OpenCV windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
